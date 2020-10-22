d3e35d5df76c481e2de8bd26c9df6f30927c720a
Patria Lim <patria.lyx@gmail.com>
2020-10-22 23:33:58 +0800

    complete Student Controller really

--- stars/StudentController.java
+++ stars/StudentController.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class StudentController {
 
-    private Student aStudent;
+    private Student myStudent;
     private CourseDB courseInfo;
     private StudentDB studentInfo;
     private StudentRegisteredCourses registeredIndex;
@@ -12,41 +12,48 @@ public class StudentController {
     public StudentController(String studentID) {
         courseInfo = new CourseDB();
         studentInfo = new StudentDB();
-        aStudent = studentInfo.loadInfo(studentID);
+        myStudent = studentInfo.loadInfo(studentID);
     }
 
     public ArrayList<Course> getCourseList() {
         return this.courseInfo.getCourseList();
     }
 
-    public boolean addIndex(Index index){
-        return aStudent.addIndex(index);
-    }
-    // can add index and drop and change index be combined?
-    public boolean dropIndex(Index index){
-        return aStudent.dropIndex(index);
-    }
-    public ArrayList<Index> getRegisteredIndex(){
-        return aStudent.getRegisteredIndex();
-    }
-    public boolean changeIndex(Index oldIndex, Index newIndex){
-        return aStudent.changeIndex(oldIndex, newIndex);
-    }
-    public boolean swopIndex(String friendID, Index myIndex) {
-        //using myIndex find myCourse, then find my friend's Index using myCourse
-        Student friend = studentInfo.loadInfo(friendID); //find the student object for your friend
-        Course myCourse = myIndex.getCourse(); //from the student object, find
-        for (Index friendIndex : friend.getRegisteredIndex()){
-            Course friendCourse = friendIndex.getCourse();
-            if (friendCourse.getCourseID()==myCourse.getCourseID()) {//id
-                Index foundIndex = friendIndex;
-                if (friend.checkChangeIndex(myIndex, friendIndex) && aStudent.checkChangeIndex(friendIndex, myIndex)) {
-                    aStudent.swopPlaces(myIndex, friendIndex);
-                    friend.swopPlaces(friendIndex, myIndex);
-                    return true;
-                }
+    public boolean addIndex(Index myIndex) {
+        for (Index existingIndex : myStudent.getRegisteredIndex()) {
+            if (existingIndex.getIndexNumber() == myIndex.getIndexNumber()) {
+                return false;
+            } else {
+                myStudent.addIndex(index);
+                return true;
             }
         }
-        return false;
+        // can add index and drop and change index be combined?
+        public boolean dropIndex (Index index){
+            return myStudent.dropIndex(index);
+        }
+        public ArrayList<Index> getRegisteredIndex () {
+            return myStudent.getRegisteredIndex();
+        }
+        public boolean changeIndex (Index oldIndex, Index newIndex){
+            return myStudent.changeIndex(oldIndex, newIndex);
+        }
+        public boolean swopIndex (String friendID, Index myIndex){
+            //using myIndex find myCourse, then find my friend's Index using myCourse
+            Student friend = studentInfo.loadInfo(friendID); //find the student object for your friend
+            Course myCourse = myIndex.getCourse(); //from the student object, find
+            for (Index friendIndex : friend.getRegisteredIndex()) {
+                Course friendCourse = friendIndex.getCourse();
+                if (friendCourse.getCourseID() == myCourse.getCourseID()) {//id
+                    Index foundIndex = friendIndex;
+                    if (friend.checkChangeIndex(myIndex, friendIndex) && myStudent.checkChangeIndex(friendIndex, myIndex)) {
+                        myStudent.swopPlaces(myIndex, friendIndex);
+                        friend.swopPlaces(friendIndex, myIndex);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
