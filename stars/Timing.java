ccf9a9903d995c9e090c0842bce607f298e25aa2
HiokHian <56589476+HiokHian@users.noreply.github.com>
2020-10-22 16:05:29 +0800

    Create Timing.java

---
+++ stars/Timing.java
@@ -0,0 +1,30 @@
+package stars;
+
+public class Timing {
+    private enum day {
+        MON, TUES, WED, THUR, FRI
+    }
+
+    private enum type {
+        TUT, LAB, LEC
+    }
+
+    private int start;
+    private int end;
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+}
