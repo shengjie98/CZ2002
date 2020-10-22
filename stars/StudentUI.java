89a86b0aaef2d2aa99407627847d0e15be5256cb
shengjie98 <53373032+shengjie98@users.noreply.github.com>
2020-10-22 20:06:44 +0800

    Update StudentUI.java

--- stars/StudentUI.java
+++ stars/StudentUI.java
@@ -16,34 +16,69 @@ public class StudentUI extends SelectUI{
         int i;
         System.out.println("in admin display menu");
         do {
-            System.out.println("1. Add Course Index");
-            System.out.println("2. Add student");
-            System.out.println("3. Add course");
-            System.out.println("4. Update course");
-            System.out.println("5. Check availablity slot for an index");
-            System.out.println("6. Print student list by index number");
-            System.out.println("7. Print student list by course");
+            System.out.println("1. Add course index");
+            System.out.println("2. Drop course index");
+            System.out.println("3. Print Rrgistered courses");
+            System.out.println("4. Check vacancies available");
+            System.out.println("5. Change index number of course");
+            System.out.println("6. Swop index number with another student");
             System.out.print("Option: ");
             i = sc.nextInt();
             switch (i) {
                 case 1:
                     addIndex();
                     break;
+                case 2: 
+                    dropIndex();
+                    break;
                 default:
                     break;
             }
 
-        } while (i > 0 && i < 8) ;
+        } while (i > 0 && i < 7) ;
         return;
     }
 
-    public void addIndex() {
+    private void addIndex() {
         ArrayList<Course> courseList;
         Course selectedCourse;
         ArrayList<Index> indexList;
         Index selectedIndex;
         
         courseList = studentController.getCourseList();
-        selectedCourse = (Course)select(indexList);
+        selectedCourse = (Course)select(courseList);
+        indexList = selectedCourse.getIndexList();
+        selectedIndex = (Index)select(indexList);
+        studentController.addIndex(selectedIndex);
     }
+    private void dropIndex() {
+        ArrayList<Index> indexList;
+        Index selectedIndex;
+        indexList = studentController.getRegisteredIndex();
+        selectedIndex = (Index)select(indexList);
+        studentController.dropIndex(selectedIndex);
+    }
+    
+    private void printRegisteredCourses() {
+        ArrayList<Index> indexList;
+        Index selectedIndex;
+        indexList = studentController.getRegisteredIndex();
+        for (Index index: indexList) {
+            System.out.println(index.print());
+        }
+    }
+    
+    private void checkVacanciesAvailable() {
+        ArrayList<Course> courseList;
+        Course selectedCourse;
+        ArrayList<Index> indexList;
+        Index selectedIndex;
+        
+        courseList = studentController.getCourseList();
+        selectedCourse = (Course)select(courseList);
+        indexList = selectedCourse.getIndexList();
+        selectedIndex = (Index)select(indexList);
+        System.out.println("Vacancies: ", selectedIndex.getVacancy());
+    }
+    
 }
