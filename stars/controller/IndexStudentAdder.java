a2d2bb93709e9516d438b10583fe1901e57d4241
shengjie98 <53373032+shengjie98@users.noreply.github.com>
2020-11-17 02:30:55 +0800

    Javadoc IndexStudentAdder

--- stars/controller/IndexStudentAdder.java
+++ stars/controller/IndexStudentAdder.java
@@ -2,9 +2,27 @@ package stars.controller;
 
 import stars.entity.*;
 import stars.exceptions.*;
-import stars.boundary.*;
 
+/**
+ * Class to handle logic for student adding a Index
+ */
 public class IndexStudentAdder {
+    /**
+     * Handles the logic to add students to Index
+     * @param index Index where student is added to
+     * @param studentRegisteredCourses StudentRegisteredCourses object of the studnet
+     * @return true if added to confirmed list in the index, false if added to the waitlist of index
+     * @throws ExceedAUException          If the user is unable to add the Index to
+     *                                    the list of Registered Courses because it
+     *                                    would exceed the AU limit
+     * @throws TimetableClashException    If the user is unable to add the Index to
+     *                                    the list of Registered Courses because of
+     *                                    a Timetable Clash between te Index and the
+     *                                    Student's exisiting Indexes
+     * @throws AlreadyRegisteredException If the user is unable to add the Index
+     *                                    because he has already Registered for the
+     *                                    Course
+     */
     public boolean addStudent(Index index, StudentRegisteredCourses studentRegisteredCourses) throws ExceedAUException, TimetableClashException, AlreadyRegisteredException {
         Student student = studentRegisteredCourses.getOwner();
         // check if student already has the course
