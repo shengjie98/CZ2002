0e0ace0c8d045b663c5db3d99d0e6d015647cea1
shengjie98 <53373032+shengjie98@users.noreply.github.com>
2020-11-17 01:52:19 +0800

    javadoc for database manager

--- stars/controller/DatabaseManager.java
+++ stars/controller/DatabaseManager.java
@@ -9,35 +9,67 @@ import java.util.ArrayList;
 
 import stars.entity.*;
 
+/**
+ * Database manager class to access database, handles database and hides implementation of database from client
+ */
 public class DatabaseManager {
     private Database db;
     private final String DATABASE_FILE = "stars/database.ser";
 
+    /**
+     * Constructor for DatabaseManager
+     */
     public DatabaseManager() {
         loadInformation();
     }
 
+    /**
+     * Gets all students from the database
+     * @return ArrayList of students from database
+     */
     public ArrayList<Student> getStudentArray() {
         return db.getStudentArray();
     }
 
+    /**
+     * Gets all courses from the database
+     * @return ArrayList of courses from database
+     */
     public ArrayList<Course> getCoursesArray() {
         return db.getCoursesArray();
     }
 
+    /**
+     * Adds a student to the database
+     * @param student Student object to be added to the database
+     * @return true if added successfully, false if studentID already exists in the database
+     */
     public boolean addStudent(Student student) {
         return db.addStudent(student);
     }
 
+    /**
+     * Adds a course to the database
+     * @param course Course object to be added to the database
+     * @return true if added successfullym false if courseID already exists in the database
+     */
     public boolean addCourse(Course course) {
         return db.addCourse(course);
     } 
 
+    /**
+     * Get student object from database using username
+     * @param studentID username of student to be found
+     * @return Student object with matching studentID
+     */
     public Student findStudent(String studentID) {
         return db.findStudent(studentID);
     }
 
-    public void loadInformation() {
+    /**
+     * Method to read serialised files from disk and load the database into memory
+     */
+    private void loadInformation() {
         try {
 			ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(DATABASE_FILE));
 			this.db = (Database) inputStream.readObject();
@@ -55,6 +87,9 @@ public class DatabaseManager {
 		}
     }
 
+    /**
+     * Method to write databse from memory to disk as serialised file
+     */
     public void saveInformation() {
         try {
 			ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(DATABASE_FILE));
