3801ca10ef52249590dcdf45fddb9ae88625e704
shengjie98 <53373032+shengjie98@users.noreply.github.com>
2020-11-17 15:20:55 +0800

    Update StudentController.java

--- stars/controller/StudentController.java
+++ stars/controller/StudentController.java
@@ -16,7 +16,6 @@ public class StudentController {
 
     /**
      * Constructor for StudentController
-     * 
      * @param studentID The ID of the Student
      */
     public StudentController(String studentID) {
@@ -27,7 +26,6 @@ public class StudentController {
 
     /**
      * Gets List of Courses that is in the Database
-     * 
      * @return The ArrayList of Courses in the database
      */
     public ArrayList<Course> getCourseList() {
@@ -37,7 +35,6 @@ public class StudentController {
     /**
      * Adds the Index to the Student's StudentRegisteredCourses and adds the Student
      * to the Index's List of Students
-     * 
      * @param myIndex The Index to be added to the Student's
      *                StudentRegisteredCourses
      * @return Boolean that indicates true if the Index has been added to the
@@ -54,39 +51,52 @@ public class StudentController {
      *                                      because he has already Registered for
      *                                      the Course
      */
-    public boolean addIndex(Index myIndex)
-            throws ExceedAUException, TimetableClashException, AlreadyRegisteredException {
+    public boolean addIndex(Index myIndex) throws ExceedAUException, TimetableClashException, AlreadyRegisteredException {
         return myStudent.addIndex(myIndex);
     }
 
-    public boolean dropIndex(Index index) {
+    /**
+     * Drops an Index from the Student's StudentRegisteredCourses and removes student from the Index
+     * @param index Index tht the student is dropping
+     * @return Boolean that indicates true if student is dropped from confirm list, false if student is dropped from waitlist
+     */
+    public boolean dropIndex (Index index){
         return myStudent.dropIndex(index);
     }
-
-    public ArrayList<Index> getRegisteredIndex() {
+    
+    /**
+     * Getter method to get a full list of all indexes that student has registered for
+     * @return ArrayList containing all Indexes that student has registered for
+     */
+    public ArrayList<Index> getRegisteredIndex () {
         return myStudent.getRegisteredIndex();
     }
 
-    public ArrayList<Index> getConfirmedIndex() {
+    /**
+     * Getter method to get a full list of all indexes that student has been confirmed for
+     * @return ArrayList containing all Indexes that student has been confirmed for
+     */
+    public ArrayList<Index> getConfirmedIndex () {
         return myStudent.getConfirmedIndex();
     }
 
-    public ArrayList<Index> getWaitlistedIndex() {
+    /**
+     * Getter method to get a full list of all indexes that student has added to waitlist for
+     * @return ArrayList containing all Indexes that student has added to waitlist for
+     */
+    public ArrayList<Index> getWaitlistedIndex () {
         return myStudent.getWaitlistedIndex();
     }
 
-    public boolean changeIndex(Index oldIndex, Index newIndex)
-            throws ExceedAUException, TimetableClashException, AlreadyRegisteredException {
-        return myStudent.changeIndex(oldIndex, newIndex);
-    }
-
+    
     /**
-     * Method to swap position with another student another index index
-     * 
-     * @param friendID studentID of friend who going to take the place in the index
-     * @param myIndex  The index the student is currently in
-     * @return true if myStudent is added to the confirmed list of the new index,
-     *         false if added to the waitlist of the new index
+     * Method for user to change index in a course
+     * @param oldIndex Index that student is currently registered for
+     * @param newIndex New index that Student wants to join
+     * @return Boolean that indicates true if Student has been added to the confirmed list of the new Index, false if the Student is added to the waitlist
+     * @throws ExceedAUException          If the user is unable to add the Index to
+     *                                    the list of Registered Courses because it
+     *                                    would exceed the AU limit
      * @throws TimetableClashException    If the user is unable to add the Index to
      *                                    the list of Registered Courses because of
      *                                    a Timetable Clash between te Index and the
@@ -95,23 +105,39 @@ public class StudentController {
      *                                    because he has already Registered for the
      *                                    Course
      */
-    public boolean swopIndex(String friendID, Index myIndex)
-            throws TimetableClashException, AlreadyRegisteredException {
-        // using myIndex find myCourse, then find my friend's Index using myCourse
-        Student friend = dbManager.findStudent(friendID); // find the student object for your friend
-        Course myCourse = myIndex.getCourse(); // from the student object, find
+    public boolean changeIndex (Index oldIndex, Index newIndex) throws ExceedAUException, TimetableClashException, AlreadyRegisteredException {
+        return myStudent.changeIndex(oldIndex, newIndex);
+    }
+    
+    /**
+     * Method to swap position with another student another index index
+     * @param friendID studentID of friend who going to take the place in the index
+     * @param myIndex The index the student is currently in
+     * @return true if myStudent is added to the confirmed list of the new index, false if added to the waitlist of the new index
+     * @throws TimetableClashException    If the user is unable to add the Index to
+     *                                    the list of Registered Courses because of
+     *                                    a Timetable Clash between te Index and the
+     *                                    Student's exisiting Indexes
+     * @throws AlreadyRegisteredException If the user is unable to add the Index
+     *                                    because he has already Registered for the
+     *                                    Course
+     */
+    public boolean swopIndex (String friendID, Index myIndex) throws TimetableClashException, AlreadyRegisteredException {
+        //using myIndex find myCourse, then find my friend's Index using myCourse
+        Student friend = dbManager.findStudent(friendID); //find the student object for your friend
+        Course myCourse = myIndex.getCourse(); //from the student object, find
         for (Index friendIndex : friend.getRegisteredIndex()) {
             Course friendCourse = friendIndex.getCourse();
-            if (friendCourse.getCourseID().equals(myCourse.getCourseID())) {// id
+            if (friendCourse.getCourseID().equals(myCourse.getCourseID())) {//id
                 // check that index is not the same
                 if (friendIndex.getIndexNumber() == myIndex.getIndexNumber()) {
                     throw new AlreadyRegisteredException();
                 }
-
+                
                 if (friend.checkChangeIndex(myIndex, friendIndex) && myStudent.checkChangeIndex(friendIndex, myIndex)) {
                     friend.swopPlaces(myIndex, this.myStudent);
                     return myStudent.swopPlaces(friendIndex, friend);
-                }
+                } 
             }
         }
         throw new TimetableClashException();
